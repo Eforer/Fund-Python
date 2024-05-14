89fb133a4fb1676a8ebbf1424eea8405d5071e50
Eforer <E4e.1997@gmail.com>
2024-05-14 09:02:28 -0400

    Add files via upload

---
+++ numeros-input.py
@@ -0,0 +1,29 @@
+#Ejercicio1
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
+suma = float(num1) + float(num2)
+print("La suma es:", suma)
+
+#Ejericio2
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
+resta = float(num1) - float(num2)
+print("La resta es:", resta)
+
+#Ejercicio3
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
+multiplicacion = float(num1) * float(num2)
+print("La multiplicación es:", multiplicacion)
+
+#Ejercicio4
+num1 = input("Ingrese el primer número: ")
+num2 = input("Ingrese el segundo número: ")
+division = float(num1) / float(num2)
+print("La división es:", division)
+
+#Ejercicio5
+base = input("Ingrese la base: ")
+exponente = input("Ingrese el exponente: ")
+potencia = float(base) ** float(exponente)
+print("La potencia de", base,"elevado a ",exponente,"es:", potencia)
---
+++ numeross.py
@@ -0,0 +1,8 @@
+#Ejercicio 1
+#Este ejercicio calcula la suma de dos números, num1 y num2, e imprime el resultado.
+num1 = 1
+num2 = 2
+suma = num1 + num2
+print("La suma de", num1, "y", num2, "es", suma)
+
+#Ejercicio 2
